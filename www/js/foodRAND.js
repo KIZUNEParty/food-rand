bffa02786422933b0a2e52a3894f11547071daf6
koontyza007 <79712705+koontyza007@users.noreply.github.com>
2021-08-13 05:08:10 +0700

    Update foodRAND.js

--- www/js/foodRAND.js
+++ www/js/foodRAND.js
@@ -1,20 +1,50 @@
 // fetch Json
 
-fetch("./db/main.json")
+function fet(){
+    fetch("./db/main.json")
     .then((response) => {
         return response.json()
     })
     .then((data) => {
         console.log(data)
-        
         dataFuc(data)
     })
     .catch((err) => {
         console.log("error : " + err);
     })
+}
 
 function dataFuc(data) {
-    console.log(data.shopname[0].shop)
+    const id = Math.floor(Math.random() * 4);
+    if (id == 0) {
+        document.getElementById('shop-ran').innerHTML = data.shopname[0].shop
+        document.getElementById('id').value = "0"
+        document.getElementById('random').classList.add('hidden')
+        document.getElementById('warp').classList.remove('hidden')
+    } else if (id == 1) {
+        document.getElementById('shop-ran').innerHTML = data.shopname[1].shop
+        document.getElementById('id').value = "1"
+        document.getElementById('random').classList.add('hidden')
+        document.getElementById('warp').classList.remove('hidden')
+    } else if (id == 2) {
+        document.getElementById('shop-ran').innerHTML = data.shopname[2].shop
+        document.getElementById('id').value = "2"
+        document.getElementById('random').classList.add('hidden')
+        document.getElementById('warp').classList.remove('hidden')
+    } else if (id == 3) {
+        document.getElementById('shop-ran').innerHTML = data.shopname[3].shop
+        document.getElementById('id').value = "3"
+        document.getElementById('random').classList.add('hidden')
+        document.getElementById('warp').classList.remove('hidden')
+    }
+    
 }
 
 // *** TEST เองนะ อิอิ ***
+
+
+function re() {
+    document.getElementById('warp').classList.add('hidden')
+    document.getElementById('random').classList.remove('hidden')
+
+}
