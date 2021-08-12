0ce5c8309262ecf1b65dea6428ffd547f88708f6
koontyza007 <79712705+koontyza007@users.noreply.github.com>
2021-08-13 05:15:42 +0700

    Update foodRAND.js

--- www/js/foodRAND.js
+++ www/js/foodRAND.js
@@ -1,6 +1,5 @@
-// fetch Json
 
-function fet(){
+function congrat(){
     fetch("./db/main.json")
     .then((response) => {
         return response.json()
@@ -15,33 +14,10 @@ function fet(){
 }
 
 function dataFuc(data) {
-    const id = Math.floor(Math.random() * 4);
-    if (id == 0) {
-        document.getElementById('shop-ran').innerHTML = data.shopname[0].shop
-        document.getElementById('id').value = "0"
-        document.getElementById('random').classList.add('hidden')
-        document.getElementById('warp').classList.remove('hidden')
-    } else if (id == 1) {
-        document.getElementById('shop-ran').innerHTML = data.shopname[1].shop
-        document.getElementById('id').value = "1"
-        document.getElementById('random').classList.add('hidden')
-        document.getElementById('warp').classList.remove('hidden')
-    } else if (id == 2) {
-        document.getElementById('shop-ran').innerHTML = data.shopname[2].shop
-        document.getElementById('id').value = "2"
-        document.getElementById('random').classList.add('hidden')
-        document.getElementById('warp').classList.remove('hidden')
-    } else if (id == 3) {
-        document.getElementById('shop-ran').innerHTML = data.shopname[3].shop
-        document.getElementById('id').value = "3"
-        document.getElementById('random').classList.add('hidden')
-        document.getElementById('warp').classList.remove('hidden')
-    }
+
+    // สำหรับสุมอาหาร
     
 }
-
-// *** TEST เองนะ อิอิ ***
-
 
 function re() {
     document.getElementById('warp').classList.add('hidden')
