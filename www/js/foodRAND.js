220537b149e9a7e382b4d11ad48d2fbfd24ce4f9
Detzz <25049577+ProfDetermined@users.noreply.github.com>
2021-08-13 12:16:44 +0700

    updated foodrand

--- www/js/foodRAND.js
+++ www/js/foodRAND.js
@@ -84,9 +84,116 @@ function dataFuc(data) {
 
         const a = data.shopname[2]
 
+        if (rand_b == 0) {
+            menu.classList.add('hidden')
+            tb.innerHTML = a.item[0].b
+        } else if (rand_b == 1) {
+            menu.classList.add('hidden')
+            tb.innerHTML = a.item[1].b
+        } else if (rand_b == 2) {
+            menu.classList.add('hidden')
+            tb.innerHTML = a.item[2].b
+        }
+
+        if (rand_c == 0) {
+            menu.classList.add('hidden')
+            tc.innerHTML = a.item[0].c
+        } else if (rand_c == 1) {
+            menu.classList.add('hidden')
+            tc.innerHTML = a.item[1].c
+        } else if (rand_c == 2) {
+            menu.classList.add('hidden')
+            tc.innerHTML = a.item[2].c
+        } else if (rand_c == 3) {
+            menu.classList.add('hidden')
+            tc.innerHTML = a.item[3].c
+        }
+
         if (rand_a == 0) {
             menu.classList.add('hidden')
-            ta.innerHTML = a.item[0]
+            ta.innerHTML = a.item[0].a
+        } else if (rand_a == 1) {
+            menu.classList.add('hidden')
+            ta.innerHTML = a.item[1].a
+        } else if (rand_a == 2) {
+            menu.classList.add('hidden')
+            ta.innerHTML = a.item[2].a
+        } else if (rand_a == 3) {
+            menu.classList.add('hidden')
+            ta.innerHTML = a.item[3].a
+        } else if (rand_a == 4) {
+            menu.classList.add('hidden')
+            ta.innerHTML = a.item[4].a
+        } else if (rand_a == 5) {
+            menu.classList.add('hidden')
+            ta.innerHTML = a.item[5].a
+            const aa = Math.floor(Math.random() * 2)
+            if (aa == 0) {
+                tb.innerHTML = "แห้ง"
+                tc.innerHTML = ""
+            } else if (aa == 1) {
+                tb.innerHTML = "ทะเล"
+                tc.innerHTML = ""
+            }
+        } else if (rand_a == 6) {
+            menu.classList.add('hidden')
+            tb.innerHTML = ""
+            tc.innerHTML = ""
+        }
+    } else if (id == 3) {
+        const item = data.shopname[3].item[0]
+        const rand_a = Math.floor(Math.random() * 7)
+        const rand_c = Math.floor(Math.random() * 2)
+
+        menu.classList.add('hidden')
+        document.getElementById('fix').innerHTML = item.fix
+
+        if (rand_a == 0) {
+            const a = Math.floor(Math.random() * 15)
+
+            ta.innerHTML = item.a[0].a
+
+            if (a == 0) {
+                tb.innerHTML = item.b[0].a
+            } else if (a == 1) {
+                tb.innerHTML = item.b[1].a
+            } else if (a == 2) {
+                tb.innerHTML = item.b[2].a
+            } else if (a == 3) {
+                tb.innerHTML = item.b[3].a
+            } else if (a == 4) {
+                tb.innerHTML = item.b[4].a
+            } else if (a == 5) {
+                tb.innerHTML = item.b[5].a
+            } else if (a == 6) {
+                tb.innerHTML = item.b[6].a
+            } else if (a == 7) {
+                tb.innerHTML = item.b[7].a
+            } else if (a == 8) {
+                tb.innerHTML = item.b[8].a
+            } else if (a == 9) {
+                tb.innerHTML = item.b[9].a
+            } else if (a == 10) {
+                tb.innerHTML = item.b[10].a
+            } else if (a == 11) {
+                tb.innerHTML = item.b[11].a
+            } else if (a == 12) {
+                tb.innerHTML = item.b[12].a
+            } else if (a == 13) {
+                tb.innerHTML = item.b[13].a
+            } else if (a == 14) {
+                tb.innerHTML = item.b[14].a
+            } else if (a == 15) {
+                tb.innerHTML = item.b[15].a
+            }
+        }
+
+        if (rand_c == 0) {
+            menu.classList.add('hidden')
+            tc.innerHTML = item.c[0].a
+        } else if(rand_c == 1) {
+            menu.classList.add('hidden')
+            tc.innerHTML = item.c[1].a
         }
     }
 
